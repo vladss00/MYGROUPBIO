cc6b7167f16e580fe7974d040e83a79c65afb05d
Marjoriee1 <jhingmupas@gmail.com>
2025-09-02 13:10:17 +0800

    added my part

---
+++ resources/views/bio3.blade.php
@@ -0,0 +1,67 @@
+<x-layout>
+  <x-slot name="title">My Biography | College Student</x-slot>
+<div class="container">
+    <div class="header">
+      <img src="Photo1.jpeg" alt="photo1">
+      <div>
+        <h1>Marjorie Mupas Morgate</h1>
+        <p>Email: jhingmupas@gmail.com<br>
+           Phone: 09386219379<br>
+           Location: Mindoro, Bangar La Union</p>
+      </div>
+    </div>
+
+    <div class="section">
+      <h2>About Me</h2>
+      <p>Hello everyone I'm Marjorie Mupas Morgate, 20 years old, From Mindoro Bangar La Union.I am an online seller with an interest in information technology. I enjoy selling online and managing products.and i believe life is a journey, not a race</p>
+    </div>
+
+    <div class="section">
+      <h2>Skills</h2>
+      <ul>
+        <li>Data Entry and Encoding</li>
+        <li>Online Selling</li>
+      </ul>
+    </div>
+
+    <div class="section">
+      <h2>Hobbies</h2>
+      <ul>
+        <li>Social Media Marketing</li>
+        <li>Listening to Music</li>
+      </ul>
+    </div>
+  </div>
+
+    <div class="header">
+      <img src="Photo1.jpeg" alt="photo1">
+      <div>
+        <h1>Marjorie Mupas Morgate</h1>
+        <p>Email: jhingmupas@gmail.com<br>
+           Phone: 09386219379<br>
+           Location: Mindoro, Bangar La Union</p>
+      </div>
+    </div>
+
+    <div class="section">
+      <h2>About Me</h2>
+      <p>Hello everyone I'm Marjorie Mupas Morgate, 20 years old, From Mindoro Bangar La Union.I am an online seller with an interest in information technology. I enjoy selling online and managing products.and i believe life is a journey, not a race</p>
+    </div>
+
+    <div class="section">
+      <h2>Skills</h2>
+      <ul>
+        <li>Data Entry and Encoding</li>
+        <li>Online Selling</li>
+      </ul>
+    </div>
+
+    <div class="section">
+      <h2>Hobbies</h2>
+      <ul>
+        <li>Social Media Marketing</li>
+        <li>Listening to Music</li>
+      </ul>
+    </div>
+  </div>
+</x-layout>
